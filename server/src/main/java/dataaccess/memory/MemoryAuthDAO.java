93ddc47077503a4e4ae7690d15803ccc02e8cf71
pgilstru <penelope.gilstrap@gmail.com>
2025-03-01 12:57:07 -0700

    MemoryDAO updates

--- server/src/main/java/dataaccess/memory/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/memory/MemoryAuthDAO.java
@@ -1,4 +1,10 @@
 package dataaccess.memory;
 
+import model.AuthData;
+import java.util.Map;
+import java.util.HashMap;
+
 public class MemoryAuthDAO {
+    // implementation of data access interface that stores server's data in main memory RAM
+    private final Map<String, AuthData> authMap = new HashMap<>();
 }
