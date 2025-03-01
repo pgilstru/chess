cc6f01f07345d1fa2d56edaac47f2bc66c41fde9
pgilstru <penelope.gilstrap@gmail.com>
2025-03-01 12:23:34 -0700

    GameDAO and UserDAO updates

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -2,5 +2,27 @@ package dataaccess;
 
 import model.GameData;
 
+import java.util.Collection;
+
 public interface GameDAO {
+    // A method for clearing all data from the database.
+    // DataAccessException is thrown if the data can't be accessed (for any reason)
+    void clear() throws DataAccessException;
+
+    // Retrieve a specified game with the given game ID.
+    // DataAccessException is thrown if the data can't be accessed (for any reason)
+    GameData getGame(int gameID) throws DataAccessException;
+
+    // Create a new game.
+    // DataAccessException is thrown if the data can't be accessed (for any reason)
+    GameData createGame(GameData gameData) throws DataAccessException;
+
+    // Retrieve all games.
+    // DataAccessException is thrown if the data can't be accessed (for any reason)
+// possibly change this to a list instead of collection??
+    Collection<GameData> listGames() throws DataAccessException;
+
+    // Updates a chess game.
+    // DataAccessException is thrown if the data can't be accessed (for any reason)
+    void updateGame(GameData gameData) throws DataAccessException;
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -3,4 +3,15 @@ package dataaccess;
 import model.UserData;
 
 public interface UserDAO {
+    // A method for clearing all data from the database.
+    // DataAccessException is thrown if the data can't be accessed (for any reason)
+    void clear() throws DataAccessException;
+
+    // Retrieve a user with the given username.
+    // DataAccessException is thrown if the data can't be accessed (for any reason)
+    UserData getUser(UserData username) throws DataAccessException;
+
+    // Create a new user.
+    // DataAccessException is thrown if the data can't be accessed (for any reason)
+    void createUser(UserData userData) throws DataAccessException;
 }
