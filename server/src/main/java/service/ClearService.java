0fbb3a4afd732a0528ebda6d102ea600fbb6cf2e
pgilstru <penelope.gilstrap@gmail.com>
2025-03-02 12:45:15 -0700

    more progress in service classes

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,4 +1,31 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+import dataaccess.AuthDAO;
+
 public class ClearService {
+    private final UserDAO userDAO;
+    private final AuthDAO authDAO;
+    private final GameDAO gameDAO;
+
+    public ClearService(UserDAO userDAO, AuthDAO authDAO, GameDAO gameDAO) {
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+    }
+
+    // public void clear() {}
+    public void clear() {
+        try {
+            // clear user data, auth data, and game data
+            userDAO.clear();
+            authDAO.clear();
+            gameDAO.clear();
+        } catch (DataAccessException e) {
+            // change this from runtime exception to something better?
+            throw new RuntimeException(e);
+        }
+    }
 }
--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,4 +1,36 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import model.GameData;
+
 public class GameService {
+    private GameDAO gameDAO;
+    private AuthDAO authDAO;
+
+    public GameService(GameDAO gameDAO, AuthDAO authDAO) {
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+    }
+
+    // public CreateResult create(CreateRequest createRequest) {}
+    public GameData create(GameData gameName, String authToken) {
+        try {
+            // verify user is authenticated first
+            if (authDAO.getAuth(authToken) == null) {
+                throw new IllegalArgumentException("Must be authenticated to logout");
+            }
+
+            // create a new game
+            gameDAO.createGame(gameName);
+
+
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // public JoinResult join(JoinRequest joinRequest) {}
+    // public ListResult list(ListRequest listRequest) {}
 }
