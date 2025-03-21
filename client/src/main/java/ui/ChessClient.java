db0df7a8850ad58eceb0894d1c8cabc2665e6ff4
pgilstru <penelope.gilstrap@gmail.com>
2025-03-21 12:03:57 -0600

    more serverfacade work

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,16 +1,6 @@
 package ui;
 
-import com.sun.nio.sctp.NotificationHandler;
-import server.ServerFacade;
-
 public class ChessClient {
 
-    private final ServerFacade server;
-    private final String serverUrl;
 
-    public ChessClient(String serverUrl, NotificationHandler notificationHandler) {
-        server = new ServerFacade(serverUrl);
-        this.serverUrl = serverUrl;
-        this.notificationHandler = notificationHandler;
-    }
 }
--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,12 +1,34 @@
 package server;
 
+import model.GameData;
+import model.JoinRequest;
+
 public class ServerFacade {
 
     private final String serverUrl;
 
     public ServerFacade(String url) {
-        serverUrl = url;
+        this.serverUrl = url;
     }
 
+    // createGame
+    public GameData createGame(GameData gameData) {
+        var path = "/create";
+        return this.makeRequest("POST", path, gameData, GameData.class);
+    }
+
+    // joinGame
+    public void joinGame(JoinRequest joinRequest) {
+        var path = "/join";
+        this.makeRequest("PUT", path, joinRequest, null);
+    }
+
+    // listGames
+    public List<GameData> listGames() {
+        var path = "/games";
+        record listPetResponse(List<GameData> gameDataList) {
+        }
+        return this.makeRequest("GET", path, null, listPetResponse.class);
+    }
 
 }
