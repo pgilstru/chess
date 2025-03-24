cf0207a2302baa9cac00640c2b402ae6381767cb
pgilstru <penelope.gilstrap@gmail.com>
2025-03-24 12:23:34 -0600

    more chessclient

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,6 +1,14 @@
 package ui;
 
+import server.ServerFacade;
+
 public class ChessClient {
 
+    private final ServerFacade server;
+    private final String serverUrl;
 
+    public ChessClient(String serverUrl) {
+        server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+    }
 }
